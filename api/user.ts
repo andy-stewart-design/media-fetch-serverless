913a15f29ec5c060e5a1790c5965917ba889643c
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2024-01-04 07:05:28 -0500

    fetch attempt 2

--- api/user.ts
+++ api/user.ts
@@ -1,8 +1,13 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import fetch from "node-fetch";
 
-export default (req: VercelRequest, response: VercelResponse): void => {
-  fetch("https://dummyjson.com/products")
-    .then((results) => results.json())
-    .then((data) => response.json(data));
+export default async (req: VercelRequest, res: VercelResponse) => {
+  try {
+    const response = await fetch("https://dummyjson.com/products");
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error fetching Figma data:", error);
+    res.status(500).json({ error: "Failed to fetch Figma data" });
+  }
 };
